bc327efdd1064d7d2917b4ab9703031e3750d68a
root <hadrienhachez@hotmail.com>
2017-04-02 00:03:32 +0000

    Cleaning up code

--- BBB/lib/main.py
+++ BBB/lib/main.py
@@ -9,14 +9,6 @@ led = LED.LED('gpio60','60')
 print('ok2')
 #gpio.low()
 led.Toggle(1)
-# Activate GPIO P9_12
+#led.On()
+#led.Off()
 print("ok3")
-
-#if os.path.exists('/sys/class/gpio/gpio60'):
-	#print("ok3")
-	#with open('/sys/class/gpio/export', 'w') as file:
-		#file.write('60')
-	#with open('/sys/class/gpio/gpio60/direction', 'w') as file:
-		#file.write('out')
-	#with open('/sys/class/gpio/gpio60/value', 'w') as file:
-		#file.write('0')
