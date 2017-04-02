8f62ceb89724d4e2a5a0967b0b309915866bf559
JeanNSHUTI <16186@ecam.be>
2017-04-02 02:42:16 +0200

    Update newmain.py
    Added comment for clarification of findings

--- BBB/newmain.py
+++ BBB/newmain.py
@@ -1,6 +1,9 @@
 from lib import Servo
 import time
 
+#The PWM signal on the BBB can only be generated when the board
+#is supplied with a 5V DC PSU at P1 or on pins P9_5 ou P9_6
+
 print('ok')
 servoSG90 = Servo.Servo('P9_42','20000000','2000000')
 print('ok1')
